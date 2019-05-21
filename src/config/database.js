92f76f6b3d64bb76b90515501de9184fa31f42dd
cristhian caldas <c.caldas.m@gmail.com>
2019-05-20 21:46:51 -0500

    modificando la cadena de conexion a la BD

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,10 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
-const URI = 'mongodb+srv://apirest:apirest@cluster0-rqsae.mongodb.net/tallerninja?';
+//const URI = 'mongodb+srv://apirest:apirest@cluster0-rqsae.mongodb.net/tallerninja?';
+const URI = 'mongodb://cristhian:mendoza87@ds111963.mlab.com:11963/nodetest';
 
 mongoose.connect(URI)
-.then(db=>console.log('DB CONNECT'))
-.catch(err=>console.log(err));
+    .then(db => console.log('DB CONNECT'))
+    .catch(err => console.log(err));
 
-module.exports=mongoose;
+module.exports = mongoose;
